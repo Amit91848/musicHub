498188bb3648f131511946b3051ac1b8a0ab3a72
Amit Sharma <74371312+Amit91848@users.noreply.github.com>
2024-04-30 14:05:06 +0000

    disable redis temp

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -6,7 +6,7 @@ function isRedisConnected() {
 
 export async function findInCache(key: string) {
     if (isRedisConnected()) {
-
+        return null;
         const value = await redisClient.get(key);
 
         return value;
@@ -15,12 +15,14 @@ export async function findInCache(key: string) {
 
 export async function setCache(key: string, value: string) {
     if (isRedisConnected()) {
+        return null;
         return await redisClient.set(key, value);
     } else return null;
 }
 
 export async function setExCache(key: string, value: string, expiry = 60 * 60 * 24) { // 1 day
     if (isRedisConnected()) {
-        return await redisClient.SETEX(key, expiry, value);
+        // return await redisClient.SETEX(key, expiry, value);
+        return null;
     } else return null
 }
